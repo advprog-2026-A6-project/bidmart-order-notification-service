74231de5996d0784e8c18506ebdfa74bfe15eeba
Affandi21 <affandishafwansoleh@gmail.com>
2026-03-06 01:20:44 +0700

    fix sonarcloud issue

--- src/main/java/id/ac/ui/cs/advprog/ordernotification/service/OrderServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/ordernotification/service/OrderServiceImpl.java
@@ -24,7 +24,6 @@ public class OrderServiceImpl implements OrderService {
 
         Order savedOrder = orderRepository.save(order);
 
-        //Integrasi ke Notification
         notificationService.createOrderNotification(savedOrder);
 
         return savedOrder;
